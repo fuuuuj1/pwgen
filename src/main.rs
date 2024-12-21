2a68be09569f11be74e923bf004891d5ee7cb718
fuuuu <sanchan.fuu@gmail.com>
2024-12-21 17:47:20 +0900

    feat: 入力値の受け取り

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+// まずはシンプルに入力値を受け取って表示するだけ
+#[derive(Parser, Debug)]
+#[command(version, about = "gen password", long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+
+    #[arg(short, long, default_value = "1")]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    
+    for _ in 0..args.count {
+        println!("Hello, {}!", args.name);
+    }
 }
