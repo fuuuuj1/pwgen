927be968deb4abddcfc7110d02b8df9c391d8928
fuuuu <sanchan.fuu@gmail.com>
2024-12-21 17:57:27 +0900

    feat: パスワード生成に指定するオプションの受け取り

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,22 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about = "gen password", long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    name: String,
+    #[arg(short, long, default_value = "16")]
+    length: u8,
 
+    // 生成パスワードに数値を含めるか
+    #[arg(short, long, default_value = "false")]
+    digit: bool,
+
+    // 大文字あり/なし
+    #[arg(short, long, default_value = "false")]
+    uppercase: bool,
+
+    // 記号あり/なし
+    #[arg(short, long, default_value = "false")]
+    symbols: bool,
+
+    // 生成するパスワードの数
     #[arg(short, long, default_value = "1")]
     count: u8,
 }
@@ -15,6 +28,6 @@ fn main() {
     let args = Args::parse();
     
     for _ in 0..args.count {
-        println!("Hello, {}!", args.name);
+        println!("Hello, {}!", args.length);
     }
 }
